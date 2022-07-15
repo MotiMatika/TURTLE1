4c277e061a6820cbbc65735be082c014d63e7a7b
MotiMatika <98161728+MotiMatika@users.noreply.github.com>
2022-07-15 14:35:43 +0300

    Create t1.py

---
+++ t1.py
@@ -0,0 +1,252 @@
+#ריבוע
+# import turtle               # allows us to use the turtles library
+# tw = turtle.Screen()        # creates a graphics window 
+# moti = turtle.Turtle()      # create a turtle named moti 
+# moti.color("green")           
+# moti.pensize(10) 
+
+# moti.fd(100)
+# moti.rt(90)
+# moti.fd(100)
+# moti.rt(90)
+# moti.fd(100)
+# moti.rt(90)
+# moti.fd(100)
+  
+# turtle.mainloop()           # prevent the window from closing 
+
+
+#מלבן
+# import turtle               # allows us to use the turtles library
+# tw = turtle.Screen()        # creates a graphics window 
+# moti = turtle.Turtle()      # create a turtle named moti 
+# moti.color("green")
+# moti.pensize(4)
+# moti.shape("turtle")
+# moti.speed(1)
+
+# moti.fd(150)
+# moti.rt(90)
+# moti.fd(100)
+# moti.rt(90)
+# moti.fd(150)
+# moti.rt(90)
+# moti.fd(100)
+  
+# turtle.mainloop()           # prevent the window from closing 
+
+
+
+
+# #משולש שוה צלעות
+# import turtle               # allows us to use the turtles library
+# tw = turtle.Screen()        # creates a graphics window 
+# moti = turtle.Turtle()      # create a turtle named moti 
+# moti.color("green")
+# moti.pensize(4)
+# moti.shape("turtle")
+# moti.speed(1)
+# moti.fd(150)
+
+# moti.rt(120)
+# moti.fd(150)
+# moti.rt(120)
+# moti.fd(150)
+
+# turtle.mainloop()
+
+# #מחומש משוכלל
+# import turtle               # allows us to use the turtles library
+# tw = turtle.Screen()        # creates a graphics window 
+# moti = turtle.Turtle()      # create a turtle named moti 
+# moti.color("green")
+# moti.pensize(4)
+# moti.shape("turtle")
+# moti.speed(1)
+
+# moti.fd(100)
+# moti.rt(72)
+# moti.fd(100)
+# moti.rt(72)
+# moti.fd(100)
+# moti.rt(72)
+# moti.fd(100)
+# moti.rt(72)
+# moti.fd(100)
+
+# turtle.mainloop()
+
+
+
+#משושה משוכלל
+# import turtle               # allows us to use the turtles library
+# tw = turtle.Screen()        # creates a graphics window 
+# moti = turtle.Turtle()      # create a turtle named moti 
+# moti.color("green")
+# moti.pensize(4)
+# moti.shape("turtle")
+# moti.speed(1)
+
+# moti.fd(100)
+# moti.rt(60)
+# moti.fd(100)
+# moti.rt(60)
+# moti.fd(100)
+# moti.rt(60)
+# moti.fd(100)
+# moti.rt(60)
+# moti.fd(100)
+# moti.rt(60)
+# moti.fd(100)
+
+# turtle.mainloop()
+
+
+
+#משולש שוה צלעות עם מילוי צבע
+# import turtle               # allows us to use the turtles library
+# tw = turtle.Screen()        # creates a graphics window 
+# moti = turtle.Turtle()      # create a turtle named moti 
+# moti.color("green")
+# moti.pensize(4)
+# moti.shape("turtle")
+# moti.speed(1)
+
+# moti.begin_fill()
+# moti.forward(100)
+# moti.lt(120) 
+# moti.forward(100) 
+# moti.lt(120) 
+# moti.forward(100) 
+# moti.end_fill()
+
+# turtle.mainloop()
+
+#עיגול 
+# import turtle               # allows us to use the turtles library
+# tw = turtle.Screen()        # creates a graphics window 
+# moti = turtle.Turtle()      # create a turtle named moti 
+# moti.color("green")
+# moti.pensize(4)
+# moti.shape("turtle")
+# moti.speed(1)
+
+# moti.circle(50) 
+
+# turtle.mainloop()
+
+
+#עיגול במילוי צבע
+# import turtle               # allows us to use the turtles library
+# tw = turtle.Screen()        # creates a graphics window 
+# moti = turtle.Turtle()      # create a turtle named moti 
+# moti.color("green")
+# moti.pensize(4)
+# moti.shape("turtle")
+# moti.speed(1)
+
+# moti.begin_fill()
+# moti.circle(50) 
+# moti.end_fill()
+
+# turtle.mainloop()
+
+
+
+
+#שילוב של 2 פונקציות יוצרות ריבוע ומשולש מחוברים
+#בית ירוק -ריבוע עם גג אדום-משולש שווה צלעות מלא
+# import turtle               # allows us to use the turtles library
+# tw = turtle.Screen()        # creates a graphics window 
+# moti = turtle.Turtle()      # create a turtle named moti 
+# moti.color("green")           
+# moti.pensize(4) 
+# moti.speed(1)
+# moti.shape("classic")
+# #הריבוע
+# moti.fd(80)
+# moti.rt(90)
+# moti.fd(80)
+# moti.rt(90)
+# moti.fd(80)
+# moti.rt(90)
+# moti.fd(80)
+
+# #המשולש
+
+# moti.color("red")
+
+# moti.begin_fill()
+# moti.rt(60)
+# moti.fd(50)
+# moti.rt(60) 
+# moti.fd(50)  
+# moti.end_fill()
+  
+# turtle.mainloop()           # prevent the window from closing
+# 
+print("\n")
+length=int(input("enter the length of..."))
+
+import turtle               # allows us to use the turtles library
+tw = turtle.Screen()        # creates a graphics window 
+moti = turtle.Turtle()      # create a turtle named moti            
+moti.pensize(4) 
+moti.speed(1)
+moti.shape("classic")
+
+#הריבוע
+moti.color("green")
+moti.begin_fill()
+moti.fd(length)
+moti.rt(90)
+moti.fd(length)
+moti.rt(90)
+moti.fd(length)
+moti.rt(90)
+moti.fd(length)
+moti.end_fill()
+
+moti.color("red")
+moti.begin_fill()
+moti.fd(length)
+moti.rt(90)
+moti.fd(length)
+moti.rt(90)
+moti.fd(length)
+moti.rt(90)
+moti.fd(length)
+moti.end_fill()
+
+moti.color("blue")
+moti.begin_fill()
+moti.fd(length)
+moti.lt(90)
+moti.fd(length)
+moti.lt(90)
+moti.fd(length)
+moti.end_fill()
+
+moti.color("purple")
+moti.begin_fill()
+moti.rt(90)
+moti.fd(length)
+moti.lt(90)
+moti.fd(length)
+moti.lt(90)
+moti.fd(length)
+moti.end_fill()
+
+
+moti.color("pink")
+moti.begin_fill()
+moti.rt(90)
+moti.fd(length)
+moti.lt(90)
+moti.fd(length)
+moti.lt(90)
+moti.fd(length)
+moti.end_fill()
+
+
+turtle.mainloop()           # prevent the window from closing  
